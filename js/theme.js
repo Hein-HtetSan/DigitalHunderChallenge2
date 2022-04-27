85bb5b7425e52ce12d6bb1eb57e9271850c93918
Hein-HtetSan <heinhtetsan33455@gmail.com>
2022-04-27 11:36:25 +0630

    0.5.5

--- js/theme.js
+++ js/theme.js
@@ -9,15 +9,11 @@ iconChange.onclick = function() {
         logo1.src = "./img/logo-for-light.svg"
         logo2.src = "./img/logo-for-light.svg"
         localStorage.setItem('sun', JSON.stringify(iconChange.innerHTML))
-        localStorage.setItem('logo11', JSON.stringify(logo1.src))
-        localStorage.setItem('logo22', JSON.stringify(logo2.src))
     } else {
         iconChange.innerHTML = `<i class="fa-solid fa-sun sun-icon"></i>`
         logo1.src = "./img/logo-for-dark copy.svg"
         logo2.src = "./img/logo-for-dark copy.svg"
         localStorage.setItem('moon', JSON.stringify(iconChange.innerHTML))
-        localStorage.setItem('logo1-dark', JSON.stringify(logo1.src))
-        localStorage.setItem('logo2-dark', JSON.stringify(logo2.src))
     }
     localStorage.setItem('change-theme', JSON.stringify(toLight))
 }
@@ -34,18 +30,4 @@ if(sum) {
 const moon = JSON.parse(localStorage.getItem('moon'))
 if(moon) {
     moon = iconChange.innerHTML = `<i class="fa-solid fa-moon moon-icon"></i>`
-}
-const logo1_light = JSON.parse(localStorage.getItem('logo11'))
-const logo2_light = JSON.parse(localStorage.getItem('logo22'))
-const logo1_dark = JSON.parse(localStorage.getItem('logo1-dark'))
-const logo2_dark = JSON.parse(localStorage.getItem('logo2-dark'))
-if (logo1_light && logo2_light) {
-    logo2_light = logo2.src = "./img/logo-for-light.svg"
-    logo1_light = logo1.src = "./img/logo-for-light.svg"
-}
-if (logo1_dark){
-    logo1_dark = logo1.src = "./img/logo-for-dark copy.svg"
-}
-if (logo2_dark){
-    logo2_dark = logo2.src = "./img/logo-for-dark copy.svg"
 }
