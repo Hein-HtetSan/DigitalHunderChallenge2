f9e84210fd0b284a0c96ab1f88e3a1646237221a
Hein-HtetSan <heinhtetsan33455@gmail.com>
2022-04-28 21:39:37 +0630

    0.8.0

--- js/scrollreveal.js
+++ js/scrollreveal.js
@@ -7,25 +7,25 @@ ScrollReveal({
     duration: 2000,
     delay: 400
     });
-ScrollReveal().reveal(pageOneHeader, {delay: 500, origin: 'right', distance: '30px'});
-ScrollReveal().reveal('.h2', {delay: 500, origin: 'left'});
-ScrollReveal().reveal('.p2', {delay: 500, origin: 'left', distance: '50px'});
-ScrollReveal().reveal('.first-name', {delay: 500, origin: 'top', distance: '40px'});
-ScrollReveal().reveal('.last-name', {delay: 500, origin: 'top', distance: '40px'});
-ScrollReveal().reveal('.email', {delay: 500, origin: 'top', distance: '40px'});
-ScrollReveal().reveal('.text-area', {delay: 500, origin: 'top', distance: '40px'});
-ScrollReveal().reveal('.send-btn', {delay: 500, origin: 'right', distance: '40px'});
-ScrollReveal().reveal('.btns1', {delay: 500, origin: 'left', distance: '60px'});
-ScrollReveal().reveal('.h5', {delay: 500, origin: 'right', distance: '50px'});
-ScrollReveal().reveal('.p1', {delay: 500, origin: 'bottom', distance: '40px'});
-ScrollReveal().reveal('.p2-p', {delay: 500, origin: 'right', distance: '30px'});
-ScrollReveal().reveal('.img1', {delay: 500, rotate: {x: 50, z: 70}, distance: '60px'});
-ScrollReveal().reveal('.map-info', {delay: 500, rotate: {y: 90}, distance: '50px'});
-ScrollReveal().reveal('.first-card', {delay: 500, origin: 'bottom', distance: '60px', interval: 300});
-ScrollReveal().reveal('.f-sub-card', {delay: 500, origin: 'bottom', distance: '60px', interval: 300});
-ScrollReveal().reveal('.join-now-btn', {delay: 500, origin: 'left', distance: '50px', interval: 200});
-ScrollReveal().reveal('.media', {delay: 500, origin: 'left', distance: '50px', interval: 300});
-ScrollReveal().reveal('.header', {delay: 500, origin: 'left', distance: '50px', interval: 300});
-ScrollReveal().reveal('.item', {delay: 500, origin: 'top', distance: '60px', interval: 400});
+ScrollReveal().reveal(pageOneHeader, {delay: 500, origin: 'bottom' });
+ScrollReveal().reveal('.h2', {delay: 500, origin: 'top'});
+ScrollReveal().reveal('.p2', {delay: 500, origin: 'top' });
+ScrollReveal().reveal('.first-name', {delay: 500, origin: 'top' });
+ScrollReveal().reveal('.last-name', {delay: 500, origin: 'top' });
+ScrollReveal().reveal('.email', {delay: 500, origin: 'top' });
+ScrollReveal().reveal('.text-area', {delay: 500, origin: 'top' });
+ScrollReveal().reveal('.send-btn', {delay: 500, origin: 'bottom' });
+ScrollReveal().reveal('.btns1', {delay: 500, origin: 'top' });
+ScrollReveal().reveal('.h5', {delay: 500, origin: 'bottom' });
+ScrollReveal().reveal('.p1', {delay: 500, origin: 'bottom' });
+ScrollReveal().reveal('.p2-p', {delay: 500, origin: 'bottom' });
+ScrollReveal().reveal('.img1', {delay: 500, rotate: {x: 50, z: 70} });
+ScrollReveal().reveal('.map-info', {delay: 500, rotate: {y: 90} });
+ScrollReveal().reveal('.first-card', {delay: 500, origin: 'bottom' , interval: 300});
+ScrollReveal().reveal('.f-sub-card', {delay: 500, origin: 'bottom' , interval: 300});
+ScrollReveal().reveal('.join-now-btn', {delay: 500, origin: 'top' , interval: 200});
+ScrollReveal().reveal('.media', {delay: 500, origin: 'top' , interval: 300});
+ScrollReveal().reveal('.header', {delay: 500, origin: 'top' , interval: 300});
+ScrollReveal().reveal('.item', {delay: 500, origin: 'top' , interval: 400});
 
 
